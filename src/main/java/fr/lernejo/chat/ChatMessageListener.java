82794af292ff34c87bd0ce03646f3cf090961df9
PaulGranger <paul.granger@epita.fr>
2022-01-26 12:29:39 +0100

    setup chat message repository

--- src/main/java/fr/lernejo/chat/ChatMessageListener.java
+++ src/main/java/fr/lernejo/chat/ChatMessageListener.java
@@ -2,9 +2,18 @@ package fr.lernejo.chat;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+
 @Component
 public class ChatMessageListener {
+
+    public ChatMessageRepository chatMessageRepository;
+
+    public ChatMessageListener(ChatMessageRepository chatMessageRepository) {
+        this.chatMessageRepository = chatMessageRepository;
+    }
+
     public void onMessage(String message) {
-        System.out.println(message);
+        chatMessageRepository.addChatMessage(message);
     }
 }
---
+++ src/main/java/fr/lernejo/chat/ChatMessageRepository.java
@@ -0,0 +1,24 @@
+package fr.lernejo.chat;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ChatMessageRepository {
+
+    public ArrayList<String> arrayList = new ArrayList<String>(10);
+
+    public void addChatMessage(String message) {
+        this.arrayList.add(message);
+    }
+
+    public List<String> getLastTenMessages() {
+        if (arrayList.size() < 10) {
+            return arrayList.subList(0, arrayList.size());
+        }
+
+        return arrayList.subList(Math.max(arrayList.size() - 10, 0), arrayList.size());
+    }
+}
