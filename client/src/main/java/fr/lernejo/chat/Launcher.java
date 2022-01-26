efa9aae8a37a0273054c4ef0f68458a737ac2cb3
PaulGranger <paul.granger@epita.fr>
2022-01-26 14:37:24 +0100

    ajout du rabbittemplate + envoie de message depuis le terminal

--- client/src/main/java/fr/lernejo/chat/Launcher.java
+++ client/src/main/java/fr/lernejo/chat/Launcher.java
@@ -2,21 +2,21 @@ package fr.lernejo.chat;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Scanner;
 
 @SpringBootApplication
 public class Launcher {
     public static void main(String[] args) {
+        RabbitTemplate rabbitTemplate = new AnnotationConfigApplicationContext(Launcher.class).getBean(RabbitTemplate.class);
         System.out.println("Ecris dans le terminal mon gros : touche le q pour quitter");
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
         while (!text.equals("q")) {
             System.out.println("Ecris dans le terminal mon gros : touche le q pour quitter");
-            RabbitTemplate rabbitTemplate = new RabbitTemplate();
-            //rabbitTemplate.send();
+            rabbitTemplate.convertAndSend("", "chat_messages", text);
             text = scanner.nextLine();
-
         }
         System.out.println("Salut mon pote 'sadDruckerReact'");
     }
